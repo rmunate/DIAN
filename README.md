5088597c45eb6ede540a57f33accdee09c9600b9
Grupo Altum <grupoaltum@MacBook-Pro-de-Grupo.local>
2023-06-27 09:43:30 -0500

    Adjust Format And Indexing

--- README.md
+++ README.md
@@ -1,28 +1,32 @@
-# DIGITO VERIFICACIÓN DIAN COLOMBIA (PHP | LARAVEL)
-> [![Raul Mauricio Uñate Castro](https://storage.googleapis.com/lola-web/storage_apls/RecursosCompartidos/LogoGithubLibrerias.png)](#)
+# DIGITO VERIFICACIÓN DIAN COLOMBIA (PHP & Laravel Framework) v1.x
 
-Obtenga el dígito de verificación de cualquier cédula de acuerdo con la norma de la DIAN en Colombia. 
+![Logo](https://github.com/rmunate/DIAN/assets/91748598/bc0f3516-08ab-4388-b236-2cdb27cfe40a)
 
-# Instalación
-## _Instalación a través de Composer_
+## Obtenga el dígito de verificación de cualquier cédula de acuerdo con la norma de la DIAN en Colombia. 
+
+Una forma simple, rápida y confiable de obtener el digito de verificación de cualquier NIT en Colombia, esto de acuerdo con la resolución 187620 de 2008.
+
+## _Instalacion_
 
 ```console
-composer require rmunate/dian-colombia 1.0.x-dev
+composer require rmunate/dian-colombia
 ```
 
-## Metodo
-Ingrese el numero de cedula como Integer.
+Asegúrate de que en el `composer.json` tengas la biblioteca en la última versión. `"rmunate/dian-colombia": "^1.0"`
 
+### Obtener el digito de verificación.
+```php
+//Importar Uso De Libreria.
+use Rmunate\DianColombia\DIAN;
+
+//Calcular Digito.
+$digito = DIAN::digitoVerificacion(1003618585);
+```
 | METODO | DESCRIPCIÓN |
 | ------ | ------ |
-| `DIAN::digitoVerificacion(1016080XXX)` | Retorna el dígito de verificación de la cedula de acuerdo a la norma vigente de la DIAN en Colombia. |
+| `DIAN::digitoVerificacion(1016080XXX)` | Retorna el dígito de verificación de cualquier cédula de acuerdo a la norma vigente de la DIAN en Colombia resolución 187620 de 2008. |
 
-```php
-#Este metodo solo recibe enteros
-$digitoVerificacion = DIAN::digitoVerificacion(1016080XXX);
-```
-
-## Mantenedores
-- Ingeniero, Raúl Mauricio Uñate Castro (raulmauriciounate@gmail.com)
+## Creator
+- 🇨🇴 Raúl Mauricio Uñate Castro. (raulmauriciounate@gmail.com)
 
 [![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
--- src/DIAN.php
+++ src/DIAN.php
@@ -2,64 +2,74 @@
 
 namespace Rmunate\DianColombia;
 
-/**
- * Clase para calcular digito de verificacion
- * --------------------------------------------
- * Desarrollado por: Raul Mauricio Uñate Castro
- * raulmauriciounate@gmail.com
- */
-
 class DIAN
 {
+    /**
+     * Resolucion empleada para el calculo
+     * @const string
+     */
+    const RESOLUCION = "DIAN - Resolución 187620 de 2008";
 
-    /* Tipo de Codificacion */
-    public static function digitoVerificacion(int $cedula)
+    /**
+     * Pesos segun la resolucion de la Dian.
+     * @var array
+     */
+    private $pesos = array(71, 67, 59, 53, 47, 43, 41, 37, 29, 23, 19, 17, 13, 7, 3);
+
+    /**
+     * Aloja el valor del Nit Suministrado
+     * @var [int]
+     */
+    private $nit;
+
+    /**
+     * Constructor de la Casle
+     * @param int $nit
+     */
+    public function __construct(int $nit)
     {
+        $this->nit = $nit;
+    }
 
-        /* Convertir a Entero */
-        $nit = intval($cedula);
+    /**
+     * Retorna el digito de verifiacion.
+     * @return int
+     */
+    public function digito() : int
+    {
+        
+        $pesos = $this->pesos;
+        $suma = 0;
 
-        /* Arreglo para iteracion */
-        $arr = array(
-            1 => 3,
-            4 => 17,
-            7 => 29,
-            10 => 43,
-            13 => 59,
-            2 => 7,
-            5 => 19,
-            8 => 37,
-            11 => 47,
-            14 => 67,
-            3 => 13,
-            6 => 23,
-            9 => 41,
-            12 => 53,
-            15 => 71,
-        );
-        $x = 0;
-        $y = 0;
-        $z = strlen($nit);
-        $dv = '';
+        // Convertir NIT a arreglo de dígitos
+        $digitos = array_map('intval', str_split($nit));
 
-        /* Iteraciones */
-        for ($i = 0; $i < $z; $i++) {
-            $y = substr($nit, $i, 1);
-            $x += ($y * $arr[$z - $i]);
+        // Calcular la suma de multiplicaciones
+        foreach ($digitos as $index => $digito) {
+            $suma += $digito * $pesos[$index];
         }
 
-        /* Residuo */
-        $y = $x % 11;
+        // Calcular el residuo
+        $residuo = $suma % 11;
 
-        /* Retorno de Digito de Verificacion Dian */
-        if ($y > 1) {
-            $dv = 11 - $y;
-            return $dv;
+        // Calcular el dígito de verificación
+        if ($residuo <= 1) {
+            $dv = 0;
         } else {
-            $dv = $y;
-            return $dv;
+            $dv = 11 - $residuo;
         }
 
+        return $dv;
+    }
+    /**
+     * @param int $nit
+     * 
+     * @return int
+     */
+    public static function digitoVerificacion(int $nit) : int
+    {
+        $calculo = new static($nit);
+        return $calculo->digito();
     }
 
 }
